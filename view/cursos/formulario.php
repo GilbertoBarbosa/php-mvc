d3908515eaab7227c339c5dd702ae8c665c20d25
Gilberto Barbosa <gisantos555@gmail.com>
2022-09-01 11:39:41 -0300

    Capítulo 2.1

---
+++ view/cursos/formulario.php
@@ -0,0 +1,12 @@
+<?php include __DIR__ . '/../inicio-html.php'; ?>
+
+    <form>
+        <div class="form-group">
+            <label for="descricao">Descrição</label>
+            <input type="text" id="descricao" name="descricao" class="form-control">
+
+            <button class="btn btn-primary">Salvar</button>
+        </div>
+    </form>
+
+<?php include __DIR__ . '/../fim-html.php'; ?>
--- view/inicio-html.php
+++ view/inicio-html.php
@@ -8,18 +8,5 @@
 <body>
 <div class="container">
     <div class="jumbotron">
-        <h1>Novo Curso</h1>
+        <h1><?=$titulo?></h1>
     </div>
-
-    <form>
-        <div class="form-group">
-            <label for="descricao">Descrição</label>
-            <input type="text" id="descricao" name="descricao" class="form-control">
-
-            <button class="btn btn-primary">Salvar</button>
-        </div>
-    </form>
-
-</div>
-</body>
-</html>
