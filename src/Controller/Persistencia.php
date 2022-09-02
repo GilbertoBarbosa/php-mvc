8512d603545594d92014e97bc0d9341fa2f41a8f
Gilberto Barbosa <gisantos555@gmail.com>
2022-09-02 11:02:46 -0300

    Capítulo 4

--- src/Controller/Persistencia.php
+++ src/Controller/Persistencia.php
@@ -27,8 +27,23 @@ class Persistencia implements InterfaceControladorRequisicao
         $curso = new Curso();
         $curso->setDescricao($descricao);
 
-        // Inserir no banco
-        $this->entityManager->persist($curso);
+        $id = filter_input(
+            INPUT_GET,
+            'id',
+            FILTER_VALIDATE_INT
+        );
+
+        if (!is_null($id) && $id !== false) {
+            // atualizar
+            //$curso->setId($id);
+            $curso = $this->entityManager->find(Curso::class, $id);
+            $curso->setDescricao($descricao);
+
+        } else {
+            // Inserir no banco
+            $this->entityManager->persist($curso);
+        }
+
         $this->entityManager->flush();
 
         header('Location:/listar-cursos', true, 302);
